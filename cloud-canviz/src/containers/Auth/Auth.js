dc1fbc084e7591ea7d2bc7e7712e97f1f5b960bd
Thomas Schork <tschork@ucsc.edu>
2019-10-14 16:23:09 -0700

    Made buttons to quickly move pages

--- cloud-canviz/src/containers/Auth/Auth.js
+++ cloud-canviz/src/containers/Auth/Auth.js
@@ -5,6 +5,11 @@ class Auth extends Component {
         return(
             <div>
                 LOGIN
+                {/*Test buttons used to quickly move around pages*/}
+                <br />
+                <a href="/home/test">Click here to go to Home(Test only)</a>
+                <br />
+                <a href="/Profile/test">Click here to go to Profile (Test only)</a>
             </div>
         )
     }
