30f75ff97bca13939409fd62fb52c30635cf61c5
yousef000 <ydost@ucsc.edu>
2019-10-14 17:52:48 -0700

    animations using AOC library

--- cloud-canviz/src/App.js
+++ cloud-canviz/src/App.js
@@ -4,8 +4,15 @@ import {Route, BrowserRouter} from 'react-router-dom'
 import Home from './containers/Home/Home'
 import Profile from './containers/Profile/Profile'
 import Auth from './containers/Auth/Auth'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 class App extends Component{
+  componentDidMount(){
+    AOS.init({
+      duration: 2000
+    })
+  }
   render(){
     return (
       <div>
--- cloud-canviz/src/components/DisplayArt/Art/Art.js
+++ cloud-canviz/src/components/DisplayArt/Art/Art.js
@@ -3,8 +3,8 @@ import classes from './Art.module.css';
 
 const Art = (props) => (
     <div>
-        <img className={classes.img} src={props.artURL} alt="image"/>
-        <div className={classes.artInfo}>
+        <img data-aos="zoom-in" data-aos-delay="15000"  className={classes.img} src={props.artURL} alt="image"/>
+        <div data-aos="zoom-in" className={classes.artInfo}>
             <p className={classes.username}>User Name</p>
             <p className={classes.artname}>Art Name, Date Created</p>
             <p className={classes.desc}>Description</p>
