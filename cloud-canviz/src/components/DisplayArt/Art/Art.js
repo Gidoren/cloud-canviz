338561d4b8bba4c0d107c8d88daa4c0763abce6f
yousef000 <ydost@ucsc.edu>
2019-10-16 13:17:23 -0700

    More UI stuff and renamed productslist.js to optionslist.js

--- cloud-canviz/src/components/DisplayArt/Art/Art.js
+++ cloud-canviz/src/components/DisplayArt/Art/Art.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import classes from './Art.module.css';
+import Heart from '../../../assets/images/heart.png'
 
 const Art = (props) => (
     <div>
          <img data-aos="zoom-in" data-aos-delay="15000"  className={classes.img} src={props.artURL} alt="image"/>
         <div data-aos="zoom-in" className={classes.artInfo} width={classes.img.width}>
-            <p className={classes.username}>User Name</p>
+            <p className={classes.username}>User Name <span><img className={classes.heart} src={Heart}/></span></p>
             <p className={classes.artname}>Art Name, Date Created</p>
             <p className={classes.desc}>Description</p>
             <p className={classes.dimensions}>Dimensions</p>
--- cloud-canviz/src/components/UI/SideDrawer/SideDrawer.js
+++ cloud-canviz/src/components/UI/SideDrawer/SideDrawer.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import classes from './SideDrawer.module.css';
-import ColorDots from '../../ColorPicker/ColorPicker'
-import ProductsList from '../../ProductsList/ProductsList';
+import ColorPicker from '../../ColorPicker/ColorPicker'
+import OptionsList from '../../OptionsList/OptionsList';
 
 const SideDrawer = (props) => (
     <div className={classes.SideDrawer}>
         <div className={classes.productType}>
-            <span className={classes.heading}>Product Type</span>
-            <ProductsList />
+            <span className={classes.heading}>CATEGORY</span>
+            <OptionsList option1="Option 1" option2="Option 2" option3="Option 3"/>
         </div>
         <div className={classes.color}>
-            <span className={classes.heading}>Color</span>
-            <ColorDots />
+            <span className={classes.heading}>COLOR</span>
+            <ColorPicker />
         </div>
-        
-        
+        <div className={classes.size}>
+            <span className={classes.heading}>SIZE</span>
+            <OptionsList option1="Option 1" option2="Option 2" option3="Option 3"/>
+        </div>
+        <div className={classes.price}>
+            <span className={classes.heading}>PRICE</span>
+            <OptionsList option1="Option 1" option2="Option 2" option3="Option 3"/>
+        </div>
+        <div className={classes.orientation}>
+            <span className={classes.heading}>ORIENTATION</span>
+            <OptionsList option1="Option 1" option2="Option 2" option3="Option 3"/>
+        </div>
+        <div className={classes.artistCountry}>
+            <span className={classes.heading}>ARTIST COUNTRY</span>
+            <OptionsList option1="Option 1" option2="Option 2" option3="Option 3"/>
+        </div>
     </div>
 )
 
