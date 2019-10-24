b74ffadb7cbd5168e2742d262e9b5308728ca421
Thomas Schork <tschork@ucsc.edu>
2019-10-24 15:55:27 -0700

    Got the barebone profile up and running

--- client/src/components/DisplayProfile/Top.js
+++ client/src/components/DisplayProfile/Top.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import classes from './Top.module.css'
 import Art1 from '../../assets/images/heart.png'
-import Art from '../DisplayArt/Art/Art'
 
 
 const Top = (props) => (
-    <div>
-        <img src={props.imgURL} className={classes.image} imagealt="image"></img>
-        <p ClassName={classes.name}>{props.name}</p>
+    <div className={classes.all}>
+        
+        {/*Image part of top bar*/}
+        <img src={Art1} className={classes.img} imagealt="image"></img>
+        
+        {/*Div holding info*/}
+        <div className={classes.content}>
+            <p ClassName={classes.name}>{props.name}</p>
+        </div>
     </div>
 )
 
