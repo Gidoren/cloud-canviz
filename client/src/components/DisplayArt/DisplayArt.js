c9adee44a5c2178d99f5f8bc6c6d37510ee5af8d
yousef000 <ydost@ucsc.edu>
2019-10-25 23:36:44 -0700

    getAllArts query using React-Hook userQuery which is recommended

--- client/src/components/DisplayArt/DisplayArt.js
+++ client/src/components/DisplayArt/DisplayArt.js
@@ -1,14 +1,13 @@
-import React, {Component} from 'react';
+import React from 'react';
 import classes from './DisplayArt.module.css'
 
 import {gql} from 'apollo-boost'
-import {Query} from 'react-apollo'
+import {Query, useQuery} from 'react-apollo'
 import {connect} from 'react-redux'
 
 import Spinner from '../UI/Spinner/Spinner'
 import Art1 from '../../assets/images/art.jpg'
 import Art from './Art/Art';
-import * as actionCreators from '../../store/actions'
 
 // const getArtsQuery = gql`
 //    query {
@@ -40,60 +39,43 @@ const getArtsQuery = gql`
     }
 `;
 
+const DisplayArt = () => {
 
+    /* getArtsQuery which is defined above, to get all artworks. Data, loading, error are predefined
+    in Query syntax. Data is initialized to returned Artwork Array */
+    const {data, loading, error} = useQuery(getArtsQuery) 
+    if(loading)
+        return <Spinner/>
+    if(error)
+        return <span>{console.log(error)}</span>
+    return(
+        <div className={classes.DisplayArt}>
+            <div className = {classes.row}>
+                {/* it's data.getAllArt because the query was named getAllArt in Schema.
+                Reverses Artworks to get the latest first, then loop through it to get
+                individual art. Each art have properties that we asked for in the query
+                which are defined in schema */}
+                {data.getAllArt.reverse().map(art => (
+                    <div key={art._id} className={classes.column}>
+                        {/* Art component that takes art properties as props.*/}
+                        <Art 
+                            artURL={Art1} 
+                            title={art.title}
+                            year={art.year}
+                            height={art.dimensions.height}
+                            width={art.dimensions.width}
+                            username={art.creator.username}
+                            desc={art.description}
+                            link={"/profile/"+ art.creator.username}
+                        />
+                    </div>
+                    
+                ))}
+                
+            </div>
+        </div>
 
-class DisplayArt extends Component {
-    render(){
-        return (
-            <Query query={getArtsQuery}>
-                {/* getArtsQuery which is defined above, to get all artworks. Data, loading, error are predefined
-                in Query syntax. Data is initialized to returned Artwork Array */}
-                {({data, loading, error}) => {
-                    if(loading)
-                        return <Spinner/>
-                    if(error)
-                        return <span>{console.log(error)}</span>
-                    return(
-                        <div className={classes.DisplayArt}>
-                            <div className = {classes.row}>
-                                {/* it's data.getAllArt because the query was named getAllArt in Schema.
-                                Reverses Artworks to get the latest first, then loop through it to get
-                                individual art. Each art have properties that we asked for in the query
-                                which are defined in schema */}
-                                {data.getAllArt.reverse().map(art => (
-                                    <div key={art._id} className={classes.column}>
-                                        {/* Art component that takes art properties as props.*/}
-                                        <Art 
-                                            artURL={Art1} 
-                                            title={art.title}
-                                            year={art.year}
-                                            height={art.dimensions.height}
-                                            width={art.dimensions.width}
-                                            username={art.creator.username}
-                                            desc={art.description}
-                                            link={"/profile/"+ art.creator.username}
-                                        />
-                                    </div>
-                                    
-                                ))}
-                             
-                            </div>
-                        </div>
+    )
+}
 
-                    )
-                }}
-            </Query>
-        )
-    }
-}
-const mapStateToProps = state => {
-    return{
-        
-    }
-}
-const mapDispatchToProps = dispatch => {
-    return{
-        onAddedArts: (arts) => dispatch(actionCreators.addArts(arts))
-    }
-}
-export default connect(mapStateToProps, mapDispatchToProps)(DisplayArt);
+export default DisplayArt;
