857440c6ef188242b055b8782c4ac779b3bb537e
yousef000 <ydost@ucsc.edu>
2019-11-02 01:26:10 -0700

    upload in local state array and preview image

--- client/src/components/UploadImage/UploadImage.js
+++ client/src/components/UploadImage/UploadImage.js
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, {useCallback, useState} from 'react'
 import classes from './UploadImage.module.css'
-import {useDropzone} from 'react-dropzone'
+import Dropzone, {useDropzone} from 'react-dropzone'
 import image from '../../assets/images/UploadImage.png'
+import { withPreviews } from './WithPreviews/WithPreviews'
 
 const UploadImage = (props) => {
-    const {getRootProps, getInputProps} = useDropzone()
+    const [files, setFiles] = useState([])
+    const handleDrop = useCallback(acceptedFile => {
+        setFiles([...files, ...acceptedFile])
+    })
 
     return (
-        <div className={classes.box} {...getRootProps()}>
-            <div className={classes.innerBox}>
-                <input className={classes.input} {...getInputProps()} />
-                <img className={classes.image} src={image} alt="img"/>
-                <p className={classes.p1}>Drag a file here</p>
-                <p className={classes.p2}>OR</p>
-                <p className={classes.p3}>Select a file from your computer</p>
+        <div>
+            {files.map((file, index) => (
+                <img key={index} className={classes.img} src={file.preview} />
+            ))}
+            <div className={classes.box}>
+                <Dropzone onDrop={withPreviews(handleDrop)}>
+                    {({getRootProps, getInputProps}) => (
+                        <div className={classes.innerBox} {...getRootProps()}>
+                            <input className={classes.input} {...getInputProps()} />
+                            <img className={classes.image} src={image} alt="img"/>
+                            <p className={classes.p1}>Drag a file here</p>
+                            <p className={classes.p2}>OR</p>
+                            <p className={classes.p3}>Select a file from your computer</p>
+                        </div>
+
+                    )}
+                </Dropzone>
             </div>
         </div>
     )
