80e6ed80a6819cecf1abd886e444bb66b5df7202
Thomas Schork <tschork@ucsc.edu>
2019-11-05 13:12:03 -0800

    Work done on changing between tabs on profile

--- client/src/containers/Profile/Profile.js
+++ client/src/containers/Profile/Profile.js
@@ -7,19 +7,31 @@ import Tab from "../../components/DisplayProfile/Tab";
 import DisplayArt from '../../components/DisplayArt/DisplayArt';
 
 class Profile extends Component {
+    constructor(props){
+        super(props);
+        this.state = {page: "artwork"}
+    }
     render(){
         return(
             <div>
                 <Navbar />
                 <Top name={"Thomas Schork"}/>
                 <hr />
-                <Tab option={"Artwork"}/>
-                <Tab option={"About"}/>
-                <Tab option={"Contact"}/>
+                <Tab option={"Artwork"} onkeypress="changePage(artwork)"/>
+                <Tab option={"About"} onkeypress="changePage(about)"/>
+                <Tab option={"Contact"} onkeypress="changePage(contact)"/>
                 <hr />
-                <DisplayArt />
+                {this.displayPage()}
             </div>
         )
+    }
+    changePage = (newPage) =>{
+        this.setState({page: newPage});
+    }
+    displayPage = () =>{
+        if (this.state.page === "artwork"){
+            return <DisplayArt />
+        }
     }
 }
 const mapStateToProps = state =>{ 
