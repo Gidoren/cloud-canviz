98cf6a3f75c2a666deb355db2f5d424a28007e81
yousef000 <ydost@ucsc.edu>
2019-11-05 21:52:29 -0800

    profile query works

--- client/src/containers/Profile/Profile.js
+++ client/src/containers/Profile/Profile.js
@@ -7,7 +7,17 @@ import Tab from "../../components/DisplayProfile/Tab";
 import DisplayArt from '../../components/DisplayArt/DisplayArt';
 import About from "../../components/DisplayProfile/About";
 import Contact from "../../components/DisplayProfile/Contact";
+import { gql } from "apollo-boost";
+import { Query } from "react-apollo";
 
+const getUserQuery = gql`
+  query getUserQuery($username: String) {
+    getUser(username: $username) {
+        firstName
+        lastName
+    }
+  }
+`;
 class Profile extends Component {
     constructor(props){
         super(props);
@@ -16,22 +26,34 @@ class Profile extends Component {
     render(){
         return(
             <div>
-                <Navbar />
-                <Top name={"Thomas Schork"}/>
-                <hr />
-                <div className={classes.tab}>
-                <div onClick={() => this.changePage("artwork")}>
-                    <Tab option={"Artwork"}/>
-                </div>
-                <div onClick={() => this.changePage("about")}>
-                    <Tab option={"About"}/>
-                </div>
-                <div onClick={() => this.changePage("contact")}>
-                    <Tab option={"Contact"}/>
-                </div>
-                </div>
-                <hr />
-                {this.displayPage()}
+                <Query query={getUserQuery} variables={{username: window.location.href.replace("http://localhost:3000/profile/", '')}}>
+                    {({loading, error, data}) => {
+                        if(loading) return <p>loading..</p>
+                        if(error) {console.log(error); console.log(data)}
+                        return(
+                            <div>
+                                <Navbar />
+                                <Top name={data.getUser.firstName}/>
+                                <hr />
+                                <div className={classes.tab}>
+                                <div onClick={() => this.changePage("artwork")}>
+                                    <Tab option={"Artwork"}/>
+                                </div>
+                                <div onClick={() => this.changePage("about")}>
+                                    <Tab option={"About"}/>
+                                </div>
+                                <div onClick={() => this.changePage("contact")}>
+                                    <Tab option={"Contact"}/>
+                                </div>
+                                </div>
+                                <hr />
+                                {this.displayPage()}
+
+                                </div>
+                        )
+                    }}
+                
+                </Query>
             </div>
         )
     }
