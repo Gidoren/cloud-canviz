0de3ec210013ae112c3332d0ae39e2b938b578a5
Emerson Christie <emersonjchristie@gmail.com>
2019-11-07 13:40:57 -0800

    new s3 bucket, upload working, profile working

--- client/src/components/UploadImage/S3/s3Upload.js
+++ client/src/components/UploadImage/S3/s3Upload.js
@@ -1,20 +1,16 @@
-//import React from "react";
 import AWS from "aws-sdk";
 import path from "path";
 
 const dotenv = require("dotenv");
 dotenv.config();
 
-// const AWS_ID = process.env.AWS_ACCESS_KEY_ID;
-// const AWS_ACCESS = process.env.AWS_SECRET_ACCESS_KEY;
+const AWS_ID = process.env.REACT_APP_AWS_ACCESS_KEY_ID;
+const AWS_ACCESS = process.env.REACT_APP_AWS_SECRET_ACCESS_KEY;
 
-const AWS_ID = "AKIAXEJA2EQA4A6SKF6I";
-const AWS_ACCESS = "9o7Y5T33fKW5nDjp8WNTDZkLxJiNWQ105ykdl8bE";
-
-export const uploadImage = async (filename, file, setUrl) => {
+export const uploadImage = async (filename, file) => {
   // Set the region
   AWS.config.update({
-    region: "us-west-2",
+    region: "us-west-1",
     accessKeyId: AWS_ID,
     secretAccessKey: AWS_ACCESS
   });
@@ -24,7 +20,7 @@ export const uploadImage = async (filename, file, setUrl) => {
 
   // call S3 to retrieve upload file to specified bucket
   var uploadParams = {
-    Bucket: "cloud-canvis-gallery",
+    Bucket: "cloud-canviz-photos-1",
     Key: path.basename(filename),
     Body: file,
     ACL: "public-read",
@@ -46,7 +42,6 @@ export const uploadImage = async (filename, file, setUrl) => {
     .promise()
     .then(res => {
       console.log("upload url in promise: ", res.Location);
-      //setUrl(res.Location);
       return res.Location;
     })
     .catch(err => {
@@ -55,49 +50,3 @@ export const uploadImage = async (filename, file, setUrl) => {
 };
 
 export default uploadImage;
-
-// import React from "react";
-// import AWS from "aws-sdk";
-// import path from "path";
-
-// const dotenv = require("dotenv");
-// dotenv.config();
-
-// // const AWS_ID = process.env.AWS_ACCESS_KEY_ID;
-// // const AWS_ACCESS = process.env.AWS_SECRET_ACCESS_KEY;
-
-// const AWS_ID = "AKIAXEJA2EQA4A6SKF6I";
-// const AWS_ACCESS = "9o7Y5T33fKW5nDjp8WNTDZkLxJiNWQ105ykdl8bE";
-
-// const uploadImage = props => {
-//   // Set the region
-//   AWS.config.update({
-//     region: "us-west-2",
-//     accessKeyId: AWS_ID,
-//     secretAccessKey: AWS_ACCESS
-//   });
-
-//   // Create S3 service object
-//   var s3 = new AWS.S3();
-
-//   // call S3 to retrieve upload file to specified bucket
-//   var uploadParams = {
-//     Bucket: "cloud-canvis-gallery",
-//     Key: path.basename(props.filename),
-//     Body: props.file,
-//     ACL: "public-read",
-//     ContentType: "text/plain"
-//   };
-
-//   // call S3 to retrieve upload file to specified bucket
-//   s3.upload(uploadParams, function(err, data) {
-//     if (err) console.log("Error", err);
-//     if (data) {
-//       console.log("Upload Success", data.Location);
-//       props.handleUrl(data.Location);
-//     }
-//   });
-//   return <div>SUCCESS!</div>;
-// };
-
-// export default uploadImage;
