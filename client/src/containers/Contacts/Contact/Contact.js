15b658ab6407b05510aacb07d930e758e287934b
yousef000 <ydost@ucsc.edu>
2019-11-09 21:23:37 -0800

    contacts table with pagination

---
+++ client/src/containers/Contacts/Contact/Contact.js
@@ -0,0 +1,84 @@
+import React, {Component} from 'react'
+import classes from './Contact.module.css'
+import Paper from '@material-ui/core/Paper'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+import ContactForm from './ContactForm/ContactForm'
+
+class AddContact extends Component {
+    state = {
+        contactFirstName: this.props.firstName,
+        contactLastName: this.props.lastName,
+        spouseFirstName: this.props.spouseFirstName,
+        spouseLastName: this.props.spouseLastName,
+        totalSales: this.props.totalSales,
+        email: this.props.email,
+        phoneHome: this.props.phoneHome,
+        phoneMobile: this.props.phoneMobile,
+        phoneOther: this.props.phoneOther,
+        website: this.props.website,
+        birthday: this.props.birthday,
+        company: this.props.company,
+        privateNote: this.props.privateNote,
+        streetAddress: this.props.streetAddress,
+        city: this.props.city,
+        state: this.props.state,
+        zip: this.props.zip,
+        value: 1
+    }
+    formChangeHandler = event => {
+        this.setState({[event.target.name]: event.target.value})
+    }
+    tabChangeHandler = (event, newValue) => {
+        this.setState({value: newValue})
+    }
+    render(){
+        return (
+            <div data-aos="slide-up" className={classes.addContact}>
+                <button className={classes.goBack} onClick={this.props.addContactForumHandler}>&#8249;</button>
+                <div className={classes.profileInfo}>
+                    <div>
+                        <h3 className={classes.name}>{this.state.contactFirstName} {this.state.contactLastName}</h3>
+                        <p className={classes.city}>{this.state.city}</p>  
+                    </div>
+                    <div>
+                        <p>TOTAL SALES</p>
+                        <h2 className={classes.totalSales}>${this.state.totalSales}</h2>
+                    </div>
+                </div>
+                <Paper className={classes.paper}>
+                    <Tabs
+                        value={this.state.value}
+                        onChange={this.tabChangeHandler}
+                        indicatorColor="primary"
+                        textColor="primary"
+                        centered>
+                        <Tab label="Potential Purchase" />
+                        <Tab label="Contact Info" />
+                    </Tabs>
+                </Paper>
+                {this.state.value === 1 && <ContactForm
+                                                contactFirstName={this.state.contactFirstName}
+                                                contactLastName={this.state.contactLastName}
+                                                spouseFirstName={this.state.spouseFirstName}
+                                                spouseLastName={this.state.spouseLastName}
+                                                email={this.state.email}
+                                                phoneHome={this.state.phoneHome}
+                                                phoneMobile={this.state.phoneMobile}
+                                                phoneOther={this.state.phoneOther}
+                                                website={this.state.website}
+                                                birthday={this.state.birthday}
+                                                company={this.state.company}
+                                                privateNote={this.state.privateNote}
+                                                streetAddress={this.state.streetAddress}
+                                                city={this.state.city}
+                                                state={this.state.state}
+                                                zip={this.state.zip}
+                                                formChangeHandler={this.formChangeHandler}/>}
+            </div>
+
+        )
+    }
+}
+
+export default AddContact
--- client/src/containers/Contacts/Contacts.js
+++ client/src/containers/Contacts/Contacts.js
@@ -1,56 +1,155 @@
 import React, { Component } from "react";
 import classes from "./Contacts.module.css";
-import AddContact from "./AddContact/AddContact";
+import Contact from "./Contact/Contact";
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
 
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+  },
+  tableWrapper: {
+    maxHeight: 440,
+    overflow: 'auto',
+  },
+});
+const columns = [
+  { id: 'contact', label: 'Contact', minWidth: 170 },
+  { id: 'phone', label: 'Phone', minWidth: 100 },
+  {
+    id: 'email',
+    label: 'Email',
+    minWidth: 170,
+    align: 'right',
+  },
+  {
+    id: 'leadStatus',
+    label: 'Lead Status',
+    minWidth: 170,
+    align: 'right',
+  },
+  {
+    id: 'leadValue',
+    label: 'Lead Value',
+    minWidth: 170,
+    align: 'right'
+  },
+];
+function createData(contact, phone, email, leadStatus) {
+  const l = 1000;
+  return { contact, phone, email, leadStatus, l};
+}
+const rows = [
+  createData('India', 'IN', 1324171354, 3287263),
+  createData('China', 'CN', 1403500365, 9596961),
+  createData('Italy', 'IT', 60483973, 301340),
+  createData('United States', 'US', 327167434, 9833520),
+  createData('Canada', 'CA', 37602103, 9984670),
+  createData('Australia', 'AU', 25475400, 7692024),
+  createData('Germany', 'DE', 83019200, 357578),
+  createData('Ireland', 'IE', 4857000, 70273),
+  createData('Mexico', 'MX', 126577691, 1972550),
+  createData('Japan', 'JP', 126317000, 377973),
+  createData('France', 'FR', 67022000, 640679),
+  createData('United Kingdom', 'GB', 67545757, 242495),
+  createData('Russia', 'RU', 146793744, 17098246),
+  createData('Nigeria', 'NG', 200962417, 923768),
+  createData('Brazil', 'BR', 210147125, 8515767),
+];
 class Contacts extends Component {
   state = {
-    showAddContactForum: false
+    showAddContactForum: false,
+    page: 0,
+    rowsPerPage: 10
   };
+  handleChangePage = (event, newPage) => {
+    this.setState({page: newPage})
+  }
+  handleChangeRowsPerPage = event => {
+    this.setState({rowsPerPage: +event.target.value})
+    this.setState({page: 0})
+  }
   addContactForumHandler = () => {
-    this.setState({ showAddContactForum: true });
+    this.setState(prevState => ({showAddContactForum: !prevState.showAddContactForum }));
   };
 
   render() {
     let pageToShow = (
-      <div>
-        <button
-          className={classes.button}
-          onClick={this.addContactForumHandler}
-        >
-          ADD CONTACT
-        </button>
-        <table className={classes.table}>
-          <tr className={classes.tr}>
-            <th className={classes.th}>Contact</th>
-            <th className={classes.th}>Phone</th>
-            <th className={classes.th}>Email</th>
-            <th className={classes.th}> Lead Status</th>
-            <th className={classes.th}>Lead Value</th>
-          </tr>
-          <tr className={classes.tr}>
-            <th className={classes.th}>Yusuf Dost</th>
-            <th className={classes.th}>4154809031</th>
-            <th className={classes.th}>Yusuf@gmail.com</th>
-            <th className={classes.th}>Hot</th>
-            <th className={classes.th}>$10000</th>
-          </tr>
-          <tr className={classes.tr}>
-            <th className={classes.th}>Yusuf Dost</th>
-            <th className={classes.th}>4154809031</th>
-            <th className={classes.th}>Yusuf@gmail.com</th>
-            <th className={classes.th}>Hot</th>
-            <th className={classes.th}>$10000</th>
-          </tr>
-        </table>
+      <div data-aos="slide-up">
+        <div className={classes.cover}>
+          <p className={classes.coverHeading}>Contacts</p>
+          <button
+            className={classes.button}
+            onClick={this.addContactForumHandler}>
+            ADD CONTACT
+          </button>
+        </div>
+        <Paper className={classes.root}>
+          <div className={classes.tableWrapper}>
+            <Table stickyHeader aria-label="sticky table">
+              <TableHead>
+                <TableRow>
+                  {columns.map(column => (
+                    <TableCell
+                      key={column.id}
+                      align={column.align}
+                      style={{ minWidth: column.minWidth }}
+                    >
+                      {column.label}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.slice(this.state.page * this.state.rowsPerPage, this.state.page * this.state.rowsPerPage + this.state.rowsPerPage).map(row => {
+                  return (
+                    <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                      {columns.map(column => {
+                        const value = row[column.id];
+                        return (
+                          <TableCell key={column.id} align={column.align}>
+                            {column.format && typeof value === 'number' ? column.format(value) : value}
+                          </TableCell>
+                        );
+                      })}
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </div>
+          <TablePagination
+            rowsPerPageOptions={[10, 25, 100]}
+            component="div"
+            count={rows.length}
+            rowsPerPage={this.state.rowsPerPage}
+            page={this.state.page}
+            backIconButtonProps={{
+              'aria-label': 'previous page',
+            }}
+            nextIconButtonProps={{
+              'aria-label': 'next page',
+            }}
+            onChangePage={this.handleChangePage}
+            onChangeRowsPerPage={this.handleChangeRowsPerPage}
+          />
+        </Paper>
       </div>
     );
 
     if (this.state.showAddContactForum === true) {
-      pageToShow = <AddContact 
+      pageToShow = <Contact 
                       firstName="Unnamed"
                       lastName="Contact"
                       city="Unknown City"
-                      totalSales="0.00"/>
+                      totalSales="0.00"
+                      addContactForumHandler={this.addContactForumHandler}/>
     }
     return (
       <div> 
