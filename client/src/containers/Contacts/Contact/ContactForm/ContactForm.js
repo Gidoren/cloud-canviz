8fe06542f5bbcb5db1370396e1c676f8f8861372
yousef000 <ydost@ucsc.edu>
2019-11-10 14:42:33 -0800

    created custom textfield

--- client/src/containers/Contacts/Contact/ContactForm/ContactForm.js
+++ client/src/containers/Contacts/Contact/ContactForm/ContactForm.js
@@ -1,115 +1,145 @@
 import React from 'react'
 import classes from './ContactForm.module.css'
 import TextField from '@material-ui/core/TextField'
+import { withStyles } from '@material-ui/core/styles';
 
-const ContactForm = (props) => (
-    <table data-aos="slide-up" className={classes.table}>
-        <tbody>
-            <tr className={classes.tr}>
-                <th className={classes.th}>
-                    <TextField className={classes.textField} margin="normal" variant="outlined" name="contactFirstName"
-                    label="First Name" value={props.contactFirstName !== "Unnamed"? props.contactFirstName : ""}
-                    onChange={props.formChangeHandler}
-                    />
-                </th>
-                <th className={classes.th}>
-                    <TextField className={classes.textField} margin="normal" variant="outlined" name="contactLastName" 
-                    label="Last Name" value={props.contactLastName !== "Contact"? props.contactLastName : ""}
-                    onChange={props.formChangeHandler}/>
-                </th>
-            </tr>
-            <tr className={classes.tr}>
-                <th className={classes.th}>
-                    <TextField className={classes.textField} margin="normal" variant="outlined" name="spouseFirstName" label="Spouse First Name"
-                    value={props.spouseFirtName}
-                    onChange={props.formChangeHandler}/>
-                </th>
-                <th className={classes.th}>
-                    <TextField className={classes.textField} margin="normal" variant="outlined" name="spouseLastName" label="Spouse Last Name"
-                    value={props.spouseLastName}
-                    onChange={props.formChangeHandler}/>
-                </th>
-            </tr>
-            <tr className={classes.tr}>
-                <th className={classes.th}>
-                    <TextField className={classes.textField} margin="normal" variant="outlined" name="email" label="Email"
-                    value={props.email}
-                    onChange={props.formChangeHandler}/>
-                </th>
-            </tr>
-            
-            <tr className={classes.tr}>
-                <th className={classes.th}>
-                    <TextField className={classes.textField} margin="normal" variant="outlined" name="phoneHome" label="Phone(Home)"
-                    value={props.phoneHome}
-                    onChange={props.formChangeHandler}/>
-                </th>
-                <th className={classes.th}>
-                    <TextField className={classes.textField} margin="normal" variant="outlined" name="phoneMobile" label="Phone(Mobile)"
-                    value={props.phoneMobile}
-                    onChange={props.formChangeHandler}/>
-                </th>
-                <th className={classes.th}>
-                    <TextField className={classes.textField} margin="normal" variant="outlined" name="phoneOther" label="Phone(Other)"
-                    value={props.phoneOther}
-                    onChange={props.formChangeHandler}/>
-                </th>
-            </tr>
-            <tr className={classes.tr}>
-                <th className={classes.th}>
-                    <TextField className={classes.textField} margin="normal" variant="outlined" name="company" label="Company"
-                    value={props.company}
-                    onChange={props.formChangeHandler}/>
-                </th>
-            </tr>
-            <tr className={classes.tr}>
-                <th className={classes.th}>
-                    <TextField className={classes.textField} margin="normal" variant="outlined" name="birthday" label="Birthday"
-                    value={props.birthday}
-                    onChange={props.formChangeHandler}/>
-                </th>
-                <th className={classes.th}>
-                    <TextField className={classes.textField} margin="normal" variant="outlined" name="website" label="Website"
-                    value={props.website}
-                    onChange={props.formChangeHandler}/>
-                </th>
-            </tr>
-            <tr className={classes.tr}>
-                <th className={classes.th}>
-                    <div className={classes.privateNote}>
-                        <TextField className={classes.textField} margin="normal" variant="outlined" name="privateNote" label="Private Note"
-                        d="outlined-multiline-flexible" multiline rowsMax="4"
-                        value={props.privateNote}
+const StyledTextField = withStyles(theme => ({
+    root: {
+        '& label.Mui-focused': {
+            color: '33A1DE',
+           },
+           '& .MuiInput-underline:after': {
+            borderBottomColor: '33A1DE',
+           },
+          '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+              borderColor: 'lightgrey',
+            },
+            '&:hover fieldset': {
+              borderColor: '33A1DE',
+            },
+            '&.Mui-focused fieldset': {
+              borderColor: '33A1DE',
+            },
+          }
+    },
+    label: {
+        color: theme.palette.secondary
+    }
+}))(TextField);
+
+const ContactForm = (props) => { 
+    return (
+        <table data-aos="slide-up" className={classes.table}>
+            <tbody>
+                <tr className={classes.tr}>
+                    <th className={classes.th}>
+                        <StyledTextField inputProps={{ style: { fontFamily: 'nunito', color: 'grey'}}} className={classes.textField} margin="normal" variant="outlined" name="contactFirstName"
+                        label="First Name" value={props.contactFirstName !== "Unnamed"? props.contactFirstName : ""}
+                        onChange={props.formChangeHandler}
+                        />
+                    </th>
+                    <th className={classes.th}>
+                        <TextField className={classes.textField} margin="normal" variant="outlined" name="contactLastName" 
+                        label="Last Name" value={props.contactLastName !== "Contact"? props.contactLastName : ""}
                         onChange={props.formChangeHandler}/>
-                    </div>
-                </th>
-            </tr>
-            <tr className={classes.tr}>
-                <th className={classes.th}>
-                    <TextField className={classes.textField} margin="normal" variant="outlined" name="streetAddress" label="Street Address"
-                    value={props.streetAddress}
-                    onChange={props.formChangeHandler}/>
-                </th>
-                <th className={classes.th}>
-                    <TextField className={classes.textField} margin="normal" variant="outlined" name="city" label="City"
-                    value={props.city !== "Unknown City"? props.city : ""}
-                    onChange={props.formChangeHandler}/>
-                </th>
-            </tr>
-            <tr className={classes.tr}>
-                <th className={classes.th}>
-                    <TextField className={classes.textField} margin="normal" variant="outlined" name="state" label="State"
-                    value={props.state}
-                    onChange={props.formChangeHandler}/>
-                </th>
-                <th className={classes.th}>
-                    <TextField className={classes.textField} margin="normal" variant="outlined" name="zip" label="Zip"
-                    value={props.zip}
-                    onChange={props.formChangeHandler}/>
-                </th>
-            </tr>
-        </tbody>
-    </table>
-)
+                    </th>
+                </tr>
+                <tr className={classes.tr}>
+                    <th className={classes.th}>
+                        <TextField className={classes.textField} margin="normal" variant="outlined" name="spouseFirstName" label="Spouse First Name"
+                        value={props.spouseFirtName}
+                        onChange={props.formChangeHandler}/>
+                    </th>
+                    <th className={classes.th}>
+                        <TextField className={classes.textField} margin="normal" variant="outlined" name="spouseLastName" label="Spouse Last Name"
+                        value={props.spouseLastName}
+                        onChange={props.formChangeHandler}/>
+                    </th>
+                </tr>
+                <tr className={classes.tr}>
+                    <th className={classes.th}>
+                        <TextField className={classes.textField} margin="normal" variant="outlined" name="email" label="Email"
+                        value={props.email}
+                        onChange={props.formChangeHandler}/>
+                    </th>
+                </tr>
+                
+                <tr className={classes.tr}>
+                    <th className={classes.th}>
+                        <TextField className={classes.textField} margin="normal" variant="outlined" name="phoneHome" label="Phone(Home)"
+                        value={props.phoneHome}
+                        onChange={props.formChangeHandler}/>
+                    </th>
+                    <th className={classes.th}>
+                        <TextField className={classes.textField} margin="normal" variant="outlined" name="phoneMobile" label="Phone(Mobile)"
+                        value={props.phoneMobile}
+                        onChange={props.formChangeHandler}/>
+                    </th>
+                    <th className={classes.th}>
+                        <TextField className={classes.textField} margin="normal" variant="outlined" name="phoneOther" label="Phone(Other)"
+                        value={props.phoneOther}
+                        onChange={props.formChangeHandler}/>
+                    </th>
+                </tr>
+                <tr className={classes.tr}>
+                    <th className={classes.th}>
+                        <TextField className={classes.textField} margin="normal" variant="outlined" name="company" label="Company"
+                        value={props.company}
+                        onChange={props.formChangeHandler}/>
+                    </th>
+                </tr>
+                <tr className={classes.tr}>
+                    <th className={classes.th}>
+                        <TextField className={classes.textField} margin="normal" variant="outlined" name="birthday" label="Birthday"
+                        value={props.birthday}
+                        onChange={props.formChangeHandler}/>
+                    </th>
+                    <th className={classes.th}>
+                        <TextField className={classes.textField} margin="normal" variant="outlined" name="website" label="Website"
+                        value={props.website}
+                        onChange={props.formChangeHandler}/>
+                    </th>
+                </tr>
+                <tr className={classes.tr}>
+                    <th className={classes.th}>
+                        <div className={classes.privateNote}>
+                            <TextField className={classes.textField} margin="normal" variant="outlined" name="privateNote" label="Private Note"
+                            d="outlined-multiline-flexible" multiline rowsMax="4"
+                            value={props.privateNote}
+                            onChange={props.formChangeHandler}/>
+                        </div>
+                    </th>
+                </tr>
+                <tr className={classes.tr}>
+                    <th className={classes.th}>
+                        <TextField className={classes.textField} margin="normal" variant="outlined" name="streetAddress" label="Street Address"
+                        value={props.streetAddress}
+                        onChange={props.formChangeHandler}/>
+                    </th>
+                    <th className={classes.th}>
+                        <TextField className={classes.textField} margin="normal" variant="outlined" name="city" label="City"
+                        value={props.city !== "Unknown City"? props.city : ""}
+                        onChange={props.formChangeHandler}/>
+                    </th>
+                </tr>
+                <tr className={classes.tr}>
+                    <th className={classes.th}>
+                        <TextField className={classes.textField} margin="normal" variant="outlined" name="state" label="State"
+                        value={props.state}
+                        onChange={props.formChangeHandler}/>
+                    </th>
+                    <th className={classes.th}>
+                        <TextField className={classes.textField} margin="normal" variant="outlined" name="zip" label="Zip"
+                        value={props.zip}
+                        onChange={props.formChangeHandler}/>
+                    </th>
+                </tr>
+            </tbody>
+        </table>
+
+    )
+    
+}
 
 export default ContactForm
