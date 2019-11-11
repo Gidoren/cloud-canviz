a933eae075cbd4b0005580beabfd2dc72aa755b2
yousef000 <ydost@ucsc.edu>
2019-11-10 20:10:22 -0800

    apollo createContact query

--- client/src/containers/Contacts/Contact/Contact.js
+++ client/src/containers/Contacts/Contact/Contact.js
@@ -4,6 +4,9 @@ import Paper from '@material-ui/core/Paper'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
 import ContactForm from './ContactForm/ContactForm'
+import gql from 'graphql-tag'
+import * as Query from '../../../grqphql/mutations'
+import { Mutation } from 'react-apollo';
 
 class Contact extends Component {
     state = {
@@ -34,52 +37,72 @@ class Contact extends Component {
     tabChangeHandler = (event, newValue) => {
         this.setState({value: newValue})
     }
+    saveChangesHandler = (event, createContact) => {
+        event.preventDefault()
+        createContact({
+            variable: {
+                contactInput: {
+                    firstName: this.state.firstName,
+                    lastName: this.state.lastName,
+                    phone_number: this.state.phoneHome,
+                    email: this.state.email
+                }
+            }
+        })
+
+    }
     render(){
         return (
-            <div data-aos="slide-up" className={classes.addContact}>
-                <button className={classes.goBack} onClick={this.props.addContactForumHandler}>&#8249;</button>
-                <div className={classes.profileInfo}>
-                    <div>
-                        <h3 className={classes.name}>{this.state.contactFirstName} {this.state.contactLastName}</h3>
-                        <p className={classes.city}>{this.state.city}</p>  
+            <Mutation mutation={Query.CREATE_CONTACT}>
+                {(createContact, {data}) => (
+                    <div data-aos="slide-up" className={classes.addContact}>
+                        <button className={classes.goBack} onClick={this.props.addContactForumHandler}>&#8249;</button>
+                        <div className={classes.profileInfo}>
+                            <div>
+                                <h3 className={classes.name}>{this.state.contactFirstName} {this.state.contactLastName}</h3>
+                                <p className={classes.city}>{this.state.city}</p>  
+                            </div>
+                            <div>
+                                <p>TOTAL SALES</p>
+                                <h2 className={classes.totalSales}>${this.state.totalSales}</h2>
+                            </div>
+                        </div>
+                        <Paper className={classes.paper}>
+                            <Tabs
+                                value={this.state.value}
+                                onChange={this.tabChangeHandler}
+                                indicatorColor="primary"
+                                textColor="primary"
+                                centered>
+                                <Tab label="Potential Purchase" />
+                                <Tab label="Contact Info" />
+                            </Tabs>
+                        </Paper>
+                        {this.state.value === 1 && <ContactForm
+                                                        contactFirstName={this.state.contactFirstName}
+                                                        contactLastName={this.state.contactLastName}
+                                                        spouseFirstName={this.state.spouseFirstName}
+                                                        spouseLastName={this.state.spouseLastName}
+                                                        email={this.state.email}
+                                                        phoneHome={this.state.phoneHome}
+                                                        phoneMobile={this.state.phoneMobile}
+                                                        phoneOther={this.state.phoneOther}
+                                                        website={this.state.website}
+                                                        birthday={this.state.birthday}
+                                                        company={this.state.company}
+                                                        privateNote={this.state.privateNote}
+                                                        streetAddress={this.state.streetAddress}
+                                                        city={this.state.city}
+                                                        state={this.state.state}
+                                                        zip={this.state.zip}
+                                                        formChangeHandler={this.formChangeHandler}/>}
+                        {this.state.showSaveChanges && <button className={classes.saveChangesButton} data-aos="slide-down">SAVE CHANGES</button>}
                     </div>
-                    <div>
-                        <p>TOTAL SALES</p>
-                        <h2 className={classes.totalSales}>${this.state.totalSales}</h2>
-                    </div>
-                </div>
-                <Paper className={classes.paper}>
-                    <Tabs
-                        value={this.state.value}
-                        onChange={this.tabChangeHandler}
-                        indicatorColor="primary"
-                        textColor="primary"
-                        centered>
-                        <Tab label="Potential Purchase" />
-                        <Tab label="Contact Info" />
-                    </Tabs>
-                </Paper>
-                {this.state.value === 1 && <ContactForm
-                                                contactFirstName={this.state.contactFirstName}
-                                                contactLastName={this.state.contactLastName}
-                                                spouseFirstName={this.state.spouseFirstName}
-                                                spouseLastName={this.state.spouseLastName}
-                                                email={this.state.email}
-                                                phoneHome={this.state.phoneHome}
-                                                phoneMobile={this.state.phoneMobile}
-                                                phoneOther={this.state.phoneOther}
-                                                website={this.state.website}
-                                                birthday={this.state.birthday}
-                                                company={this.state.company}
-                                                privateNote={this.state.privateNote}
-                                                streetAddress={this.state.streetAddress}
-                                                city={this.state.city}
-                                                state={this.state.state}
-                                                zip={this.state.zip}
-                                                formChangeHandler={this.formChangeHandler}/>}
-                {this.state.showSaveChanges && <button className={classes.saveChangesButton} data-aos="slide-down">SAVE CHANGES</button>}
-            </div>
+    
 
+                )}
+            </Mutation>
+            
         )
     }
 }
